2938f6968e760292f11dae967eeb8d535a518220
Ben <benawalls@gmail@gmail.com>
2016-05-15 13:21:26 -0700

    Initial commit

---
+++ templates/footer.php
@@ -0,0 +1,31 @@
+
+<!-- Footer -->
+<footer id="footer">
+
+  <ul class="icons">
+    <li><a href="#" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
+    <li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
+    <li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>
+    <li><a href="#" class="icon circle fa-github"><span class="label">Github</span></a></li>
+    <li><a href="/credits.php" >Credits</a></li>
+  </ul>
+
+  <ul class="copyright">
+    <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+  </ul>
+
+</footer>
+
+</div>
+
+<!-- Scripts -->
+<script src="js/vendor/jquery.js"></script>
+<script src="js/jquery.validate.js"></script>
+<script src="js/foundation.min.js"></script>
+
+<script>
+  $(document).foundation();
+</script>
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?php echo $page_title ?></title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <script src="js/vendor/modernizr.js"></script>
+</head>
+<body>
+<div id="contentWrapper">
+
+  <!-- Header -->
+  <header id="header" >
+    <h1 id="logo"><a href="/index.php">Twenty <span>by HTML5 UP</span></a></h1>
+
+    <nav id="nav">
+      <div class="menu-button">Menu</div>
+          <ul>
+            <li><a href="/aboutus.php">About Us</a></li>
+            <li><a href="/contactus.php">Contact Us</a></li>
+            <li><a href="/products.php">Products</a></li>
+            <li><a href="/blog.php">Blog</a></li>
+            <li><a href="/calendar.php">Calendar</a></li>
+            <li><a href="/articles.php">Articles</a></li>
+            <li><a href="/preferences.php">Preferences</a></li>
+            <li class="submenu">
+              <a href="#">Submenu</a>
+              <ul>
+                <li><a href="#">Dolore Sed</a></li>
+                <li><a href="#">Consequat</a></li>
+                <li><a href="#">Lorem Magna</a></li>
+                <li><a href="#">Sed Magna</a></li>
+                <li><a href="#">Ipsum Nisl</a></li>
+              </ul>
+            </li>
+          </ul>
+    </nav>
+
+  </header>
+
