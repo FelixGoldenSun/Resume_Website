d8989b345160e6556de7510529be7f82b9b48f13
Ben <benawalls@gmail@gmail.com>
2016-05-15 23:34:26 -0700

    Created the beginnings of the menu

--- templates/footer.php
+++ templates/footer.php
@@ -20,8 +20,8 @@
 
 <!-- Scripts -->
 <script src="js/vendor/jquery.js"></script>
-<script src="js/jquery.validate.js"></script>
-<script src="js/foundation.min.js"></script>
+<script src="js/vendor/what-input.min.js"></script>
+<script src="js/vendor/foundation.min.js"></script>
 
 <script>
   $(document).foundation();
